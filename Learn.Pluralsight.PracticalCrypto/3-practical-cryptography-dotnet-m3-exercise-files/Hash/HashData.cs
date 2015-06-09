e1b482290ffecfb17fc1d9331b9f39b237aaad6a
dance2die <dontbothermewithspam@gmail.com>
2015-06-09 19:38:40 -0400

    Created a common method for creating an hash

--- Learn.Pluralsight.PracticalCrypto/3-practical-cryptography-dotnet-m3-exercise-files/Hash/HashData.cs
+++ Learn.Pluralsight.PracticalCrypto/3-practical-cryptography-dotnet-m3-exercise-files/Hash/HashData.cs
@@ -1,39 +1,38 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 
 namespace CryptographyInDotNet
 {
-    public class HashData
-    {
-        public static byte[] ComputeHashSha1(byte[] toBeHashed)
-        {
-            using (var sha1 = SHA1.Create())
-            {
-                return sha1.ComputeHash(toBeHashed);
-            }
-        }
+	public class HashData
+	{
+		public static byte[] ComputeHashSha1(byte[] toBeHashed)
+		{
+			return ComputeHash<SHA1>(toBeHashed);
+		}
 
-        public static byte[] ComputeHashSha256(byte[] toBeHashed)
-        {
-            using (var sha256 = SHA256.Create())
-            {
-                return sha256.ComputeHash(toBeHashed);
-            }
-        }
+		public static byte[] ComputeHashSha256(byte[] toBeHashed)
+		{
+			return ComputeHash<SHA256>(toBeHashed);
+		}
 
-        public static byte[] ComputeHashSha512(byte[] toBeHashed)
-        {
-            using (var sha512 = SHA512.Create())
-            {
-                return sha512.ComputeHash(toBeHashed);
-            }
-        }    
+		public static byte[] ComputeHashSha512(byte[] toBeHashed)
+		{
+			return ComputeHash<SHA512>(toBeHashed);
+		}
 
-        public static byte[] ComputeHashMd5(byte[] toBeHashed)
-        {
-            using (var md5 = MD5.Create())
-            {
-                return md5.ComputeHash(toBeHashed);
-            }
-        } 
-    }
+		public static byte[] ComputeHashMd5(byte[] toBeHashed)
+		{
+			return ComputeHash<MD5>(toBeHashed);
+		}
+
+		private static byte[] ComputeHash<T>(byte[] toBeHashed) where T: HashAlgorithm
+		{
+			Type t = typeof (T);
+			using (var hasher = t.GetMethod("Create", new Type[0]).Invoke(null, null) as HashAlgorithm)
+			{
+				if (hasher != null) return hasher.ComputeHash(toBeHashed);
+			}
+			throw new ArgumentException("Could not convert type to HashAlgorithm");
+		}
+	}
 }
