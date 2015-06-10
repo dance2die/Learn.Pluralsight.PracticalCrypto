07783f02b610210dce9b5c511d5885ac528d0c8d
dance2die <dontbothermewithspam@gmail.com>
2015-06-09 20:10:42 -0400

    HMAC updated

--- Learn.Pluralsight.PracticalCrypto/3-practical-cryptography-dotnet-m3-exercise-files/HMAC/HMAC.cs
+++ Learn.Pluralsight.PracticalCrypto/3-practical-cryptography-dotnet-m3-exercise-files/HMAC/HMAC.cs
@@ -2,51 +2,49 @@
 
 namespace CryptographyInDotNet
 {
-    public class Hmac
-    {
-        private const int KeySize = 32;
+	public class Hmac
+	{
+		private const int _keySize = 32;
 
-        public static byte[] GenerateKey()
-        {
-            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
-            {
-                var randomNumber = new byte[KeySize];
-                randomNumberGenerator.GetBytes(randomNumber);
+		public static byte[] GenerateKey()
+		{
+			using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+			{
+				var randomNumber = new byte[_keySize];
+				randomNumberGenerator.GetBytes(randomNumber);
 
-                return randomNumber;
-            }
-        }
+				return randomNumber;
+			}
+		}
 
-        public static byte[] ComputeHmacsha256(byte[] toBeHashed, byte[] key)
-        {
-            using (var hmac = new HMACSHA256(key))
-            {
-                return hmac.ComputeHash(toBeHashed);
-            }
-        }
-    
-        public static byte[] ComputeHmacsha1(byte[] toBeHashed, byte[] key)
-        {
-            using (var hmac = new HMACSHA1(key))
-            {
-                return hmac.ComputeHash(toBeHashed);
-            }
-        }
+		public static byte[] ComputeHmacsha256(byte[] toBeHashed, byte[] key)
+		{
+			return ComputeHmac<HMACSHA256>(toBeHashed, key);
+		}
 
-        public static byte[] ComputeHmacsha512(byte[] toBeHashed, byte[] key)
-        {
-            using (var hmac = new HMACSHA512(key))
-            {
-                return hmac.ComputeHash(toBeHashed);
-            }
-        }
+		public static byte[] ComputeHmacsha1(byte[] toBeHashed, byte[] key)
+		{
+			return ComputeHmac<HMACSHA1>(toBeHashed, key);
+		}
 
-        public static byte[] ComputeHmacmd5(byte[] toBeHashed, byte[] key)
-        {
-            using (var hmac = new HMACMD5(key))
-            {
-                return hmac.ComputeHash(toBeHashed);
-            }
-        }
-    }
+		public static byte[] ComputeHmacsha512(byte[] toBeHashed, byte[] key)
+		{
+			return ComputeHmac<HMACSHA512>(toBeHashed, key);
+		}
+
+		public static byte[] ComputeHmacmd5(byte[] toBeHashed, byte[] key)
+		{
+			return ComputeHmac<HMACMD5>(toBeHashed, key);
+		}
+
+		private static byte[] ComputeHmac<T>(byte[] toBeHashed, byte[] key) where T: HMAC
+		{
+			var ctor = typeof (T).GetConstructor(new[] {typeof (byte[])});
+			using (var instance = ctor.Invoke(new object[] {key}) as HMAC)
+			{
+				return instance.ComputeHash(toBeHashed);
+			}
+		}
+
+	}
 }
